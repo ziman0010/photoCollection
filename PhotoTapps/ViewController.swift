4cd346703a8eac94750e54caf48ee8d445186ca6
ziman <alex.cheranov@yandex.ru>
2021-07-02 16:06:30 +0300

    Initial Commit

---
+++ PhotoTapps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoTapps
+//
+//  Created by Алексей Черанёв on 02.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
